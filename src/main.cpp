9c15aaa8ce57639b668e4b93d526d7b46b68cc4e
Kyle Vu <54590479+kylenvu@users.noreply.github.com>
2024-02-24 19:44:01 -0800

    set up x drive

--- src/main.cpp
+++ src/main.cpp
@@ -74,20 +74,31 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1,-2,3}); // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4,5,-6}); // Creates a motor group with forwards port 4 and reversed ports 4 & 6
+	// Setting up the hardware for our robot
+	// instantiating (making an instance of) motors for x-drive
+	pros::Motor m_botLeftMotor = pros::Motor(20);
+	pros::Motor m_topLeftMotor = pros::Motor(11);
+	pros::Motor m_botRightMotor = pros::Motor(10);
+	pros::Motor m_topRightMotor = pros::Motor(1);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0); // Prints status of the emulated screen LCDs
-						 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y); // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X); // Gets the turn left/right from right joystick
-		left_mg = dir - turn; // Sets left motor voltage
-		right_mg = dir + turn; // Sets right motor voltage
+
+	// instantiating a Controller object
+	pros::Controller m_controller = pros::Controller(pros::E_CONTROLLER_MASTER);
+
+	while (true) {	// forces the code to run forever
+		/* negative sign required since pushing the left_y stick up gives a value of -127
+		so, if I want the motor to run forward, I need to give it positive values
+		hence, the extra negative sign (e.g,. -(-127))
+		*/
+		int y_dir = -m_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int x_dir = -m_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+		int rot = -m_controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+		m_topLeftMotor.move_velocity(y_dir + x_dir + rot);
+		m_botLeftMotor.move_velocity(y_dir - x_dir + rot);
+		m_topRightMotor.move_velocity(-y_dir + x_dir + rot);
+		m_botRightMotor.move_velocity(-y_dir - x_dir + rot);
+
 		pros::delay(20); // Run for 20 ms then update
 	}
 }
