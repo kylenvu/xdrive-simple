10d6657789a7ca5e04e5a710f1f98a8bb5e20b97
ktvuwhs <ktvu@hbuhsd.edu>
2024-03-16 19:10:38 -0700

    included Drivetrain.hpp

--- include/main.h
+++ include/main.h
@@ -40,6 +40,7 @@
  * You should add more #includes here
  */
 //#include "okapi/api.hpp"
+#include "subsystems/Drivetrain.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
